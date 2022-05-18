388e3a08f9191e608257e5f71fce222019fb9006
NilEis <52820950+NilEis@users.noreply.github.com>
2022-05-18 23:03:57 +0200

    Bench AMD

--- 4/run.sh
+++ 4/run.sh
@@ -1,4 +1,4 @@
-gcc -std=c17 -Wall -Wextra -pedantic -fsanatize=address,undefined -pthread -o OUTFILE threads.c
+gcc -std=c17 -Wall -Wextra -pedantic -fsanitize=address,undefined -pthread -o OUTFILE threads.c
 echo Run singlethreaded
 ./OUTFILE ./matrizen/a.txt ./matrizen/b.txt 0
 echo Run multithreaded
