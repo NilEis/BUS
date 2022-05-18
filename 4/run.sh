14be37440e7968eca6167e7d26aa6832746cfb31
NilEis <52820950+NilEis@users.noreply.github.com>
2022-05-18 22:42:35 +0200

    r

--- 4/run.sh
+++ 4/run.sh
@@ -1,4 +1,4 @@
-gcc -std=c17 -Wall -Wextra -pedantic -O3 -pthread -o OUTFILE threads.c
+gcc -std=c17 -Wall -Wextra -pedantic -fsanatize=address,undefined -pthread -o OUTFILE threads.c
 echo Run singlethreaded
 ./OUTFILE ./matrizen/a.txt ./matrizen/b.txt 0
 echo Run multithreaded
